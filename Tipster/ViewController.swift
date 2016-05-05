18df9ed64a58494f1530232c16736b8620adddd5
Michael Arbogast <marboga@gmail.com>
2016-05-05 13:29:36 -0700

    numbers adding properly and displaying. tip share adjusting dynamically.

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -26,31 +26,46 @@ class ViewController: UIViewController {
     @IBOutlet weak var clearButton: UIButton!
     
     var groupSizeSelected: Int = 4
-    var tipPercentSelected: Double = 18.0
-    var billTotal: Double = 32.38
+    var tipPercentSelected: Double = 20.0
+    var billTotalString: String = "0"
+    var billTotal: Double = 0
     
     @IBAction func tipPercentSliderChanged(sender: UISlider) {
         tipPercentSelected = Double(sender.value)
         tipPercentValue.text = "Tip: \(Int(tipPercentSelected))"
-        tipPercentCollection[0].text = String.localizedStringWithFormat("%.0f %@",(tipPercentSelected - 5), "%")
-        tipPercentCollection[1].text = String.localizedStringWithFormat("%.0f %@",(tipPercentSelected), "%")
-        tipPercentCollection[2].text = String.localizedStringWithFormat("%.0f %@",(tipPercentSelected + 5), "%")
+        updateDisplayUI()
     }
     
     @IBAction func groupSizeSliderChanged(sender: UISlider) {
         groupSizeSelected = Int(sender.value)
         print(groupSizeSelected, " = groupsize,", billTotal, "=billtotal", tipPercentSelected, "=tippercent")
-        groupSizeValue.text = "Group size: \(groupSizeSelected)"
-        tipAmountCollection[0].text = String(format: "%.2f", (billTotal * (tipPercentSelected - 5) / 100))
-        tipAmountCollection[1].text = String(format: "%.2f", (billTotal * (tipPercentSelected) / 100))
-        tipAmountCollection[2].text = String(format: "%.2f", (billTotal * (tipPercentSelected + 5) / 100))
+        updateDisplayUI()
     }
     
+    @IBAction func numberButtonPressed(sender: UIButton) {
+        print(sender.tag)
+        if billTotalString == "0" {
+            if sender.tag != 10 {
+                billTotalString = String(sender.tag)
+            } else {
+                billTotalString = "."
+            }
+        } else {
+            if sender.tag != 10 {
+            billTotalString += "\(sender.tag)"
+            } else {
+                billTotalString += "."
+            }
+        }
+        updateDisplayUI()
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tipAmountCollection[0].text = "\((tipPercentSelected - 5.0) * billTotal / 100)"
+        tipAmountCollection[0].text = "\((tipPercentSelected - 5.0) * Double(billTotalString)! / 100)"
+        billTotal = 0
+        updateDisplayUI()
     }
 
     override func didReceiveMemoryWarning() {
@@ -58,7 +73,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    
+    func updateDisplayUI() {
+        resultLabel.text = billTotalString
+        groupSizeValue.text = "Group size: \(groupSizeSelected)"
+        tipAmountCollection[0].text = String(format: "%.2f", (Double(billTotalString)! * (tipPercentSelected - 5) / Double(groupSizeSelected) / 100 ))
+        tipAmountCollection[1].text = String(format: "%.2f", (Double(billTotalString)! * (tipPercentSelected) / Double(groupSizeSelected) / 100))
+        tipAmountCollection[2].text = String(format: "%.2f", (Double(billTotalString)! * (tipPercentSelected + 5) / Double(groupSizeSelected) / 100))
+        tipPercentCollection[0].text = String.localizedStringWithFormat("%.0f %@",(tipPercentSelected - 5), "%")
+        tipPercentCollection[1].text = String.localizedStringWithFormat("%.0f %@",(tipPercentSelected), "%")
+        tipPercentCollection[2].text = String.localizedStringWithFormat("%.0f %@",(tipPercentSelected + 5), "%")
+    }
     
 }
 
