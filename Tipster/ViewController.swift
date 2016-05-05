3e4c379597d65ffac94b37b73bbf2868de406e44
Michael Arbogast <marboga@gmail.com>
2016-05-05 08:17:27 -0700

    layout issues resolved. sliders updating views. issue with number of decimals resolved.

--- Tipster/ViewController.swift
+++ Tipster/ViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var resultLabel: UILabel!
     
     @IBOutlet var tipPercentCollection: [UILabel]!
@@ -18,19 +19,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipPercentValue: UILabel!
     @IBOutlet weak var groupSizeValue: UILabel!
     
+    @IBOutlet weak var tipPercentSlider: UISlider!
+    @IBOutlet weak var groupSizeSlider: UISlider!
     
     @IBOutlet var numeralCollection: [UIButton]!
     @IBOutlet weak var clearButton: UIButton!
     
-    
+    var tipPercentSelected: Double = 0.0
     @IBAction func tipPercentSliderChanged(sender: UISlider) {
-        let tipPercentSelected = Float(sender.value)
-        tipPercentValue.text = "Tip: \(tipPercentSelected)%"
+        tipPercentSelected = Double(sender.value)
+        tipPercentValue.text = String.localizedStringWithFormat("%.1f %@", "Tip: ",tipPercentSelected, "%")
+        tipPercentCollection[0].text = String.localizedStringWithFormat("%.1f %@",(tipPercentSelected - 5), "%")
+        tipPercentCollection[1].text = String.localizedStringWithFormat("%.1f %@",tipPercentSelected, "%")
+        tipPercentCollection[2].text = String.localizedStringWithFormat("%.1f %@",(tipPercentSelected + 5), "%")
     }
     
     @IBAction func groupSizeSliderChanged(sender: UISlider) {
         let groupSizeSelected = Int(sender.value)
         groupSizeValue.text = "Group Size: \(groupSizeSelected)"
+        tipAmountCollection[0].text = "\(Double(resultLabel.text!)! * (tipPercentSelected - 5) / 100  )"
     }
     
     override func viewDidLoad() {
